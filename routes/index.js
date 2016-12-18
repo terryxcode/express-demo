4f1e5716e7a9a49669c5d62f61e3040e94b9257b
terryx <terry.xu@ehealth-china.com>
2016-12-18 17:19:31 +0800

    add access filter

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', notAuth);
+
 router.get('/login', function(req, res) {
   res.render('login', { title: 'Sign in'});
 });
@@ -23,13 +25,32 @@ router.post('/login', function(req, res) {
   res.redirect('/login');
 });
 
+router.get('/logout', auth);
+
 router.get('/logout', function(req, res) {
   req.session.user = null;
   res.redirect('/');
 });
 
+router.get('/home', auth);
+
 router.get('/home', function(req, res) {
   res.render('home', { title: 'Home' });
 });
+
+function auth(req, res, next) {
+  if (!req.session.user) {
+    req.session.error = 'Please login first';
+    return res.redirect('/login');
+  }
+  next();
+}
+function notAuth(req, res, next) {
+  if (req.session.user) {
+    req.session.error = 'signed in';
+    return res.redirect('/');
+  }
+  next();
+}
 
 module.exports = router;
